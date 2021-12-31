cc0996982cabcd2736245cf4aaccf969389a3d97
fp <huang.fp@gmail.com>
2021-12-30 21:28:07 -0600

    adding files for github deploying

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
+import { CardList } from './components/card-list/card.list.component'
+import { SeachBox } from './components/search-box/seach-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField: '',
+    };
+  }
+
+  /*
+   A good rule of thumb is this: Use arrow functions on any class methods 
+   you define and aren't part of Reac*/
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
+
+  //setState causes the call of render
+  render() {
+    const { monsters, searchField } = this.state;
+    const filteredMonsters = monsters.filter(monster =>
+      monster.name.toLowerCase().includes(searchField.toLowerCase()));
+
+    return (
+      <div className="App">
+        <h1>Monsters Rolodex</h1>
+        <SeachBox 
+          placeholder='search monsters' 
+          handleChange={this.handleChange} />
+        <CardList monsters={filteredMonsters} />
+      </div>
+    );
+  }
+
+  //This is a lifecycle method
+  //the comparison of fetch again jQuery.ajax:
+  //   https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(users => this.setState({ monsters: users }));
+  }
 }
 
+
+
+//default export and named export. 
+//see: https://stackoverflow.com/questions/36426521/what-does-export-default-do-in-jsx
 export default App;
